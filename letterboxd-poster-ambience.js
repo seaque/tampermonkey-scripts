db8dfc1ac08c6001c511e507bd7f178d36cbe8d0
seaque <emirhanhasanoglu@proton.me>
2024-04-02 18:50:30 +0300

    fix letterboxd poster script

--- letterboxd-poster-ambience.js
+++ letterboxd-poster-ambience.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         Letterboxd Poster Ambience
-// @version      1.1
+// @namespace    https://github.com/seaque/tampermonkey-scripts
+// @version      1.2.1
 // @description  Ambient background color for film posters.
 // @author       seaque
 // @license       MIT
@@ -8,10 +9,10 @@
 // @downloadURL  https://github.com/seaque/tampermonkey-scripts/raw/main/letterboxd-poster-ambience.js
 // @match        *://*.letterboxd.com/film/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=letterboxd.com
-// @require      https://cdnjs.cloudflare.com/ajax/libs/color-thief/2.3.0/color-thief.umd.js
 // @grant        GM_addStyle
 // @run-at       document-start
 // ==/UserScript==
+/* globals jQuery, $, waitForKeyElements */
  
 (function() {
     'use strict';
@@ -19,75 +20,71 @@
     let imageElement;
     let isMobile = window.navigator.userAgent.toLowerCase().includes("mobi");
     
-    const rgbToHex = (r, g, b) => {
-        const hexValue = ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-        return "#" + hexValue;
-    }
- 
-    const createAmbientDiv = (color, w, h) => {
+    const createAmbientDiv = (img, w, h) => {
         const ambient_div = document.createElement('div');
         ambient_div.setAttribute('id', 'ambientDiv');
         GM_addStyle(`
             #ambientDiv {
+              position: absolute;
+              margin-left: -10px;
+            }
+            #ambientDiv img {
               width: ${w}px;
               height: ${h}px;
-              position: absolute;
+              max-width: 280px;
+              max-height: 420px;
               -webkit-filter: blur(100px);
-              background-color: ${color};
               opacity: 0;
-              animation: show 500ms 50ms cubic-bezier(0.38, 0.97, 0.56, 0.76) forwards;
+              animation: show 500ms 50ms cubic-bezier(.45,.05,.55,.95) forwards;
             }
             @keyframes show {
+              25% {
+                opacity: 0.2;
+                transform: none;
+              }
               100% {
-                opacity: 0.6;
+                opacity: 0.65;
                 transform: none;
               }
             }
-          `);
- 
+        `);
+
+        ambient_div.append(img);
         return ambient_div;
     };
- 
+
     const getPosterImage = () => {
- 
-        if (isMobile){
-            imageElement = $('div.poster-list > a#poster-zoom > div.film-poster > div > img');}
-        else {
-            imageElement = $('section.poster-list > a#poster-zoom > div.film-poster > div > img')
-        }
- 
+
+        imageElement = $('div.film-poster > div > img')
         const url = imageElement[0].getAttribute('srcset');
- 
+
         const posterImage = new Image();
         posterImage.crossOrigin = "Anonymous";
         posterImage.src = url;
- 
+
         return posterImage;
     };
- 
+
     const addAmbientDiv = () => {
         const img = getPosterImage();
-        const colorThief = new ColorThief();
-        const hexValue = rgbToHex(...colorThief.getColor(img));
-        //console.log("[POSTER AMBIENCE]", colorThief.getPalette(img));
- 
+
         img.addEventListener("load", () => {
             const width = img.naturalWidth;
             const height = img.naturalHeight;
             if (isMobile){
-                imageElement.parent().parent().parent().parent().prepend(createAmbientDiv(hexValue, width / 3, height / 3));
+                imageElement.parent().parent().parent().parent().prepend(createAmbientDiv(img, width / 5, height / 5));
             }
             else {
-                imageElement.parent().parent().parent().parent().prepend(createAmbientDiv(hexValue, width / 1.7, height / 1.7));
+                imageElement.parent().parent().parent().parent().prepend(createAmbientDiv(img, width, height));
             }
         });
     };
- 
+
     setInterval(function() {
-        if ( $('#ambientDiv').length ){
+        if ( $('#ambientDiv img').length ){
             return;
         }
         addAmbientDiv();
-    }, 750);
- 
+    }, 2500);
+
 })();
