0a52f9c90afd068baaf35d931c316cffad455c97
seaque <seaque@protonmail.com>
2025-05-07 16:07:33 +0300

    feat(lbxd): refactor

--- letterboxd-poster-ambience.js
+++ letterboxd-poster-ambience.js
@@ -1,120 +1,119 @@
 // ==UserScript==
 // @name         Letterboxd Poster Ambience
-// @namespace    https://github.com/seaque/tampermonkey-scripts
-// @version      1.3
+// @version      1.4
 // @description  Ambient background color for film posters.
 // @author       seaque
 // @license       MIT
+// @match        *://*.letterboxd.com/film/*
+// @match        *://*.letterboxd.com/*/*/film/*
+// @icon         https://a.ltrbxd.com/logos/letterboxd-decal-dots-neg-rgb.svg
 // @namespace    https://github.com/seaque/tampermonkey-scripts
 // @downloadURL  https://github.com/seaque/tampermonkey-scripts/raw/main/letterboxd-poster-ambience.js
-// @match        *://*.letterboxd.com/film/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=letterboxd.com
 // @grant        GM_addStyle
 // @run-at       document-start
 // ==/UserScript==
 /* globals jQuery, $, waitForKeyElements */
-
+    
 (function() {
     'use strict';
     
+    GM_addStyle(`
+        .ambientDiv {
+            position: absolute;
+            -webkit-filter: blur(100px);
+            filter: blur(100px);
+        }
+        .ambientDiv img {
+            width: 100%;
+            height: auto;
+            max-width: 280px;
+            max-height: 420px;
+    
+            opacity: 0;
+            animation: show 500ms 50ms cubic-bezier(.45,.05,.55,.95) forwards;
+        }
+        @keyframes show {
+            25% {
+            opacity: 0.2;
+            transform: none;
+            }
+            100% {
+            opacity: 0.85;
+            transform: none;
+            }
+        }
+    `);
+    
     let imageElement;
-    let isMobile = window.navigator.userAgent.toLowerCase().includes("mobi");
     const logPrefix = '%cLETTERBOXD POSTER AMBIENCE';
-
-    const createAmbientDiv = (img, w, h) => {
-        const ambient_div = document.createElement('div');
-        ambient_div.setAttribute('id', 'ambientDiv');
-        GM_addStyle(`
-            #ambientDiv {
-              position: absolute;
-              margin-left: -10px;
-            }
-            #ambientDiv img {
-              width: ${w}px;
-              height: ${h}px;
-              max-width: 280px;
-              max-height: 420px;
-              -webkit-filter: blur(100px);
-              opacity: 0;
-              animation: show 500ms 50ms cubic-bezier(.45,.05,.55,.95) forwards;
-            }
-            @keyframes show {
-              25% {
-                opacity: 0.2;
-                transform: none;
-              }
-              100% {
-                opacity: 0.65;
-                transform: none;
-              }
-            }
-        `);
-
-        ambient_div.append(img);
-        return ambient_div;
-    };
-
+    const logStyle = [
+        'color: white',
+        'background: linear-gradient(90deg,rgba(255, 128, 0, 1) 0%, rgba(0, 224, 84, 1) 50%, rgba(64, 188, 244, 1) 100%);',
+        'border-radius: 4px',
+        'padding: 0 4px',
+    ].join(';');
+    
     const getPosterImage = () => {
         try {
             const posterImage = new Image();
-
-            imageElement = $('a > div.film-poster > div > img');
+    
+            imageElement = $('.film-poster > div > img');
             if (!imageElement || imageElement.length === 0) {
                 throw new Error('Poster image element not found.');
             }
-
+    
             const url = imageElement[0].getAttribute('srcset');
             if (!url) {
                 throw new Error('Poster image URL not found.');
             }
-
-            console.info(logPrefix, logStyle, `Successfully fetched poster: "${url}"`);
+    
+            console.info(logPrefix, logStyle, `Fetched poster: "${url}"`);
             posterImage.crossOrigin = "Anonymous";
             posterImage.src = url;
-
+    
             return posterImage;
         } catch (error) {
-            console.error(logPrefix, logStyle, `Error fetching poster image: ${error.message}`);
+            console.error(logPrefix, logStyle, `Error fetching poster: ${error.message}`);
             return null;
         }
     };
-
+    
+    const createAmbientDom = (img, w, h) => {
+        const ambient_div = document.createElement('div');
+        ambient_div.setAttribute('class', 'ambientDiv');
+        ambient_div.append(img);
+        return ambient_div;
+    };
+    
     const addAmbientDiv = () => {
         const img = getPosterImage();
         if (!img) return;
-
+    
         img.addEventListener("load", () => {
             const width = img.naturalWidth;
             const height = img.naturalHeight;
-            if (isMobile){
-                imageElement.parent().parent().parent().parent().prepend(createAmbientDiv(img, width / 3, height / 3));
-            }
-            else {
-                imageElement.parent().parent().parent().parent().prepend(createAmbientDiv(img, width, height));
+    
+            const parent = $('.-single')
+            if (parent) {
+                console.info(logPrefix, logStyle, `Parent element:`, parent);
+                parent.prepend(createAmbientDom(img, width, height));
+            } else {
+                console.error("Parent element not found.");
             }
         });
     };
-
+    
     let funcInterval = setInterval(() => {
-        if ( $('#ambientDiv img').length ){
-            console.info(logPrefix, logStyle, 'Poster Ambience Applied.');
-            clearInterval(funcInterval);
-            funcInterval = null;
-            return;
-        }
         addAmbientDiv();
-    }, 2000);
-
-    const logStyle = [
-        'color: white',
-        'background: #445566',
-        'border: 1px solid #2c3440',
-        'border-radius: 4px',
-    ].join(';');
-
+    }, 3000);
+    
     setTimeout(() => {
         if (funcInterval) {
-            console.info(logPrefix, logStyle, 'Time Out. Exiting...');
+            if ( $('.ambientDiv img').length ) { console.info(logPrefix, logStyle, 'Poster Ambience Applied.'); }
+            else { console.info(logPrefix, logStyle, 'Time Out! Exiting.'); }
+            clearInterval(funcInterval);
+            funcInterval = null;
         }
-    }, 8000);
+    }, 6000);
 })();
+
